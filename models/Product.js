6d8a8cd9f6bbae21ba82121905ac77b1918044e6
plyn <plyn_rider@outlook.com>
2023-12-10 12:27:50 +0300

    order and product models are done

---
+++ models/Product.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema({
+  productName: String,
+  price: Number,
+  description: String,
+  image:String,
+  shop:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'Shop'
+  },
+  order:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'Order'
+  }
+},
+ {timestamps: true}
+ );
+
+ // create product model
+ const Product = mongoose.model("Product" , productSchema);
+
+ //export model
+ module.exports = {Product};
