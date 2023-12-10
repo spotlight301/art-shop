ccba2e87ae4d30dfa5e48ad077d59dfd2c448f8b
plyn <plyn_rider@outlook.com>
2023-12-10 13:55:05 +0300

    add bootstrap

--- controllers/artistpage.js
+++
@@ -1,6 +0,0 @@
-
-
-
-exports.ArtistPage = (req,res) => {
-    res.render('home/artist' , {message:" it's working in the artist page, welcome to artist website"});
-  };
---
+++ controllers/shop.js
@@ -0,0 +1,6 @@
+
+
+
+exports.ArtistPage = (req,res) => {
+    res.render('shop' , {message:" it's working in the shop page, welcome to artist website"});
+  };
