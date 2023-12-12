e210a6367910eecdf20dbaba976670bfa7394d12
plyn <plyn_rider@outlook.com>
2023-12-12 09:34:19 +0300

    added multer for shop and avatar

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,6 +2,7 @@
 const { User } = require("../models/User");
 const { Product } = require("../models/Product");
 const { Shop } = require("../models/Shop");
+const upload = require('../config/multer');
 
 
 exports.shop_index_get = (req,res) => {
@@ -18,15 +19,20 @@ exports.shop_create_get = (req,res) => {
     res.render("shop/add")
 }
 
+
 exports.shop_create_post = (req,res) => {
     console.log(req.body);
-    let shop = new Shop(req.body)
+    let shop = new Shop({
+      logo:req.file.filename,
+      name: req.body.name,
+      userId: req.user.id
+    })
 console.log("shop" , shop)
     // Save Shop
     shop.save()
     .then(() => {
-        let shopId = req.body._id;
-        res.redirect("/product/add" , shopId);
+        let shopId = shop.id;
+        res.redirect("/product/add?shopId=" + shopId);
     })
     .catch((err) => {
         console.log(err);
