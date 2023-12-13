e63e4ffdbca8f3fc121acdac559e008f9d5ca3ed
plyn <plyn_rider@outlook.com>
2023-12-13 18:05:02 +0300

    fixed BUGSS 2

--- controllers/cart.js
+++ controllers/cart.js
@@ -13,7 +13,7 @@ module.exports.cart_add_get = (req, res) => {
       },
     })
     .then((cart) => {
-      // console.log("cart details GET", cart.products)
+      console.log("cart details GET", cart.products);
       res.render("cart/index", { cart });
     })
     .catch((err) => {
--- routes/shop.js
+++ routes/shop.js
@@ -1,17 +1,32 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-const shopController = require('../controllers/shop');
-const upload = require('../config/multer'); 
+const shopController = require("../controllers/shop");
+const upload = require("../config/multer");
 
+const { authorized_user, isLogged } = require("../config/authorization");
 
 // router.get('/shop', shopController.shop_create_get);
 
-
-router.get("/add", shopController.shop_create_get);
-router.post("/add", upload.single('logo') , shopController.shop_create_post);
-router.get("/index/:id", shopController.shop_index_get);
-
+router.get(
+  "/add",
+  isLogged,
+  authorized_user(2),
+  shopController.shop_create_get
+);
+router.post(
+  "/add",
+  isLogged,
+  authorized_user(2),
+  upload.single("logo"),
+  shopController.shop_create_post
+);
+router.get(
+  "/index/:id",
+  isLogged,
+  authorized_user(2),
+  shopController.shop_index_get
+);
 
 module.exports = router;
