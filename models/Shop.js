dac8e01e06ac756778de60db2176293356baec03
plyn <plyn_rider@outlook.com>
2023-12-13 08:56:25 +0300

    resolve model/shop

---
+++ models/Shop.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const shopSchema = mongoose.Schema({
+  name: String,
+  logo: String,
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+// Module Exports
+const Shop = mongoose.model("Shop", shopSchema);
+module.exports = { Shop };
